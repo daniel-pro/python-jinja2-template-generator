c409ae42c704f5244e7a03eb51a34387faf4910f
Daniel Pro <43214013+daniel-pro@users.noreply.github.com>
2020-02-24 15:55:12 +0100

    Updated gencfgcli.py
    Updated the code to conform PEP8 standards

--- gencfgcli.py
+++ gencfgcli.py
@@ -1,35 +1,41 @@
 '''
 Created on Feb 17, 2019
 @author: Daniel Procopio
-@summary: This program invokes the class that generates a config file from a jinja2 template and a YAML config file.
+@summary: This program invokes the class that generates a config file 
+          from a jinja2 template and a YAML config file.
 @version: 1.0
 '''
 
 import argparse
-from   jinja2cfg import config
+from jinja2cfg import config
 import logging
 import sys
 
 
 def generateconfig(args):
-    newconfig = config.Cfg(args.input_yaml,args.input_template, args.output_file)
+    newconfig = config.Cfg(args.input_yaml, args.input_template,
+                           args.output_file)
     newconfig.generate()
     logger.info(newconfig.getGeneratedCfg())
     logger.info("Done")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
 
     logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s [%(name)-9s] [%(levelname)-8s] - %(message)s',
+                        format='%(asctime)s [%(name)-9s] [%(levelname)-8s]'
+                               '- %(message)s',
                         datefmt='%Y-%m-%d %H:%M')
     logger = logging.getLogger('gencfgcli')
-        
+
     main_parser = subparsers.add_parser('gencfg')
-    main_parser.add_argument('input_yaml',help='YAML file contaning the configuraton data')
-    main_parser.add_argument('input_template',help='Jinja2 template file')
-    main_parser.add_argument('output_file',help='Generated config file',default="")
+    main_parser.add_argument('input_yaml', help='YAML file contaning the'
+                                                'configuraton data')
+    main_parser.add_argument('input_template', help='Jinja2 template file')
+    main_parser.add_argument('output_file', help='Generated config file',
+                             default='')
     main_parser.set_defaults(func=generateconfig)
 
     args = parser.parse_args()
@@ -37,4 +43,4 @@ if __name__ == '__main__':
         args.func(args)
     else:
         logger.critical("Missing arguments")
-        main_parser.print_help()   
+        main_parser.print_help()
